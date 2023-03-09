4ee4fc24356774b74ca2f6915efd19beade9c850
clew <lewis@clewup.co.uk>
2023-03-09 23:25:26 +0000

    Refactoring and pickling implementation.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import pickle
+from datetime import datetime
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -5,45 +8,74 @@ from bs4 import BeautifulSoup
 
 CHROME_DRIVER_PATH = 'c:/WebDrivers/chromedriver.exe'
 
-def get_data():
+def get_data(query):
     # Set up the webdriver
     options = Options()
     driver = webdriver.Chrome(options=options, executable_path=CHROME_DRIVER_PATH)
 
-    # Fetch Indeed
-    driver.get("https://uk.indeed.com")
+    # Fetch Amazon
+    driver.get("https://amazon.co.uk")
 
-    # Fetch 'Developer' listings in the UK
-    whatInput = driver.find_element(By.ID, "text-input-what")
-    whatInput.send_keys("Developer")
-    whereInput = driver.find_element(By.ID, "text-input-where")
-    whereInput.send_keys("United Kingdom")
-    findJobsButton = driver.find_element(By.CLASS_NAME, "yosegi-InlineWhatWhere-primaryButton")
-    findJobsButton.click()
+    # Search for the query
+    search_input = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Search Amazon.co.uk"]')
+    search_input.send_keys(query)
+    search_button = driver.find_element(By.ID, "nav-search-submit-button")
+    search_button.click()
 
-    # Parse and print the results
+    # Fetch the products list
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    listings = soup.find_all("table", {"class": "jobCard_mainContent"})
-    parse_data(listings)
+    products = soup.find_all("div", {"data-component-type": "s-search-result"})
+
+    # Initialize an empty product data array
+    product_data_array = []
+
+    # Loop through and parse each listing
+    for product in products:
+        product_link = product.find("a", {"class": "a-link-normal"})
+        driver.get("https://www.amazon.co.uk" + product_link.get("href"))
+
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        product_data = parse_data(soup)
+        product_data_array.append(product_data)
 
     driver.quit()
 
+    # Pickle the data
+    save_data(query, product_data_array)
 
-def parse_data(listings):
-    for listing in listings:
-        title = listing.find("a", {"role": "button"})
-        title_text = title.text
 
-        salary = listing.find("div", {"class": "salary-snippet-container"})
-        salary_text = "Not Stated"
+class Product(object):
+    def __init__(self, name, image, price):
+        self.name = name
+        self.image = image
+        self.price = price
 
-        if salary:
-            salary_text = salary.text
 
-        f = open("salaryinfo.txt", "a+")
-        f.write("\n" + title_text + ": " + salary_text)
-        f.close()
+def parse_data(soup: BeautifulSoup):
+    # Parse key information
+    product_name = soup.find("span", {"id": "productTitle"}).text
+    product_image = soup.find("img", {"id": "landingImage"}).get("src")
+
+    # Parse the price components and concatenate together
+    product_price_symbol = soup.find("span", {"class": "a-price-symbol"}).text
+    product_price_whole = soup.find("span", {"class": "a-price-whole"}).text
+    product_price_fraction = soup.find("span", {"class": "a-price-fraction"}).text
+    product_full_price = product_price_symbol + product_price_whole + product_price_fraction
+
+    # Return the data as an object
+    return Product(
+        name=product_name.strip(),
+        image=product_image,
+        price=product_full_price)
+
+
+def save_data(query, products: []):
+    filename = query + "_" + datetime.now().strftime("%m%d%Y-%H:%M:%S") + ".pkl"
+    f = open(filename, "wb")
+    pickle.dump(products, f)
+
+    f.close()
 
 
 if __name__ == '__main__':
-    get_data()
+    get_data("Yubico")
